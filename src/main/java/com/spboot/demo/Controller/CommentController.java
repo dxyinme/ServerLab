e1a53b662e7868547a374ebecc4ca3b5f07b4c35
dxyinme <dxyinme@outlook.com>
2019-11-01 10:36:13 +0800

    change some method of comment ...

--- src/main/java/com/spboot/demo/Controller/CommentController.java
+++ src/main/java/com/spboot/demo/Controller/CommentController.java
@@ -10,21 +10,22 @@ import java.util.List;
 public class CommentController {
     OperatorComment opC = dataLinker.opComment;
     OperatorUser opU = dataLinker.opUser;
+
     @PostMapping(value = "/CommentOp/addcomment")
     @ResponseBody
     public String addComment(@RequestParam("comment") String comment,
                              @RequestParam("name") String user,
-                             @RequestParam("house_id") String house_id){
-        int suc = opC.addComment(comment , opU.askId(user) ,Integer.parseInt(house_id));
-        if(suc != 0){
+                             @RequestParam("house_id") String house_id) {
+        int suc = opC.addComment(comment, opU.askId(user), Integer.parseInt(house_id));
+        if (suc != 0) {
             return "add success";
         }
-                return "can't add";
-                }
+        return "can't add";
+    }
 
-@GetMapping(value = "/CommentInfo/askhouse/{house_id}")
-public String addComment(@PathVariable("house_id") String house_id){
+    @GetMapping(value = "/CommentInfo/askhouse/{house_id}")
+    public String addComment(@PathVariable("house_id") String house_id) {
         List<String> ret = opC.askAllComment(Integer.parseInt(house_id));
         return ret.toString();
-        }
-        }
+    }
+}
