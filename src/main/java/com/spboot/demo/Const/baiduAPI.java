7f54cb1715e13b89ce2c642cf6b3fbaff7c4974f
dxyinme <dxyinme@outlook.com>
2019-11-12 21:28:50 +0800

    slove image

--- src/main/java/com/spboot/demo/Const/baiduAPI.java
+++ src/main/java/com/spboot/demo/Const/baiduAPI.java
@@ -18,6 +18,11 @@ public class baiduAPI {
                 "&output=html&src=webapp.baidu.openAPIdemo";
     }
 
+    /**
+     *  to get the Address ' s location (lat , lng)
+     * @param Address
+     * @return a String of url to find it.
+     */
     public String getlLocationByAddress(String Address){
         return "http://api.map.baidu.com/place/v2/search?query="+
                 Address+
@@ -34,4 +39,18 @@ public class baiduAPI {
                 "&radius="+Radius.toString()+"&output=json&ak=" + AK;
     }
 
+    /**
+     * get the image around the (lat , lng).
+     * @param lat
+     * @param lng
+     * @return a image around this Location.
+     */
+    public String getImgByLocation(Double lng , Double lat){
+        return "<img style=\"margin:20px\" width=\"280\" height=\"140\" "
+                +"src=\"http://api.map.baidu.com/staticimage/v2?"
+                +"ak="+AK
+                +"&center=" + lng.toString() + "," + lat.toString()
+                +"&markers=" + lng.toString() + "," + lat.toString()
+                +"&width=280&height=140&zoom=13\"/>";
+    }
 }
--- src/main/java/com/spboot/demo/Controller/HouseInfo.java
+++ src/main/java/com/spboot/demo/Controller/HouseInfo.java
@@ -3,8 +3,7 @@ package com.spboot.demo.Controller;
 import com.spboot.demo.Const.baiduAPI;
 import com.spboot.demo.House_service.HouseService;
 import com.spboot.demo.LogicAPI.Urlcov.Urlcov;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -14,13 +13,24 @@ public class HouseInfo {
     HouseService opH = dataLinker.opHouse;
     baiduAPI bua = dataLinker.baiduapi;
     Urlcov urlcatcher = dataLinker.urlcov;
-    @GetMapping(value="/HouseInfo/open/QueryMapCircle/{address}")
-    public String QueryMapCircle(@PathVariable("address") String address){
+
+    /*
+     * 调用方式
+     *
+     *
+     * /HouseInfo/open/QueryMapCircle?address=地名
+     * 返回一个<img>
+     * sample:
+     * <img style="margin:20px" width="280" height="140" src="http://api.map.baidu.com/staticimage/v2?ak=E4805d16520de693a3fe707cdc962045&width=280&height=140&zoom=1"/>
+     *
+     */
+    @RequestMapping(value="/HouseInfo/open/QueryMapCircle")
+    public String QueryMapCircle(String address){
         String url = bua.getlLocationByAddress(address);
         System.out.println(url);
         Map<String,Double> mert = urlcatcher.getPosition(url);
-        System.out.println(mert.get("lat") + " " + mert.get("lng"));
-
-        return url;
+        System.out.println(mert.get("lng") + " " + mert.get("lat"));
+        System.out.println(bua.getImgByLocation(mert.get("lng") , mert.get("lat")));
+        return bua.getImgByLocation(mert.get("lng") , mert.get("lat"));
     }
 }
