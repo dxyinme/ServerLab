bce989bd831ed6a222a72471722e7baa8a03683a
dxyinme <dxyinme@outlook.com>
2020-04-04 21:32:48 +0800

    # swagger finished

--- src/main/java/com/spboot/demo/Const/CONSTLIST.java
+++ src/main/java/com/spboot/demo/Const/CONSTLIST.java
@@ -1,8 +1,12 @@
 package com.spboot.demo.Const;
 
-public class const_oct {
+public class CONSTLIST {
     static public String admin = "root";
     static public String password = "13705034784";
     static public Integer INF = 1000000000;
     static public String mysqlIP = "114.115.169.56";
+
+    static public int OK = 200;
+    static public int FAIL = 210;
+
 }
--- src/main/java/com/spboot/demo/Controller/UserController.java
+++ src/main/java/com/spboot/demo/Controller/UserController.java
@@ -2,15 +2,37 @@ package com.spboot.demo.Controller;
 
 
 
+import com.spboot.demo.Const.CONSTLIST;
+import com.spboot.demo.HttpResponse.HttpResponse;
+import com.spboot.demo.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
 
 
+@Api(tags={"用户系统"})
 @RestController
 @CrossOrigin // 允许外部访问
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+
+
+    @ApiOperation(value = "登录" , response = HttpResponse.class)
+    @PostMapping("usersystem/login")
+    public HttpResponse Login(
+            HttpServletRequest request,
+            @RequestParam(value = "username") String username,
+            @RequestParam(value = "password") String password
+    ){
+        System.err.println(username + " " + password);
+        return new HttpResponse(CONSTLIST.OK , "login success");
+    }
 
 }
